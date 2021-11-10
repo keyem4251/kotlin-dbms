db6bd5a46d3db230f7c6a2f6a36ee339d659558f
keyem4251 <kisk.emura@gmail.com>
2021-11-10 21:58:17 +0900

    kdoc log manager

--- src/main/kotlin/simpledb/log/LogManager.kt
+++ src/main/kotlin/simpledb/log/LogManager.kt
@@ -6,7 +6,15 @@ import simpledb.file.Page
 
 /**
  * システムの最初に作成される
+ * ログをバイト配列として扱い、FileMangerを通してディスクに保存（メモリにログの内容を保持する場合はPage）
+ * クライアントへはLogIteratorとしてログの内容をバイト配列として返す
  *
+ * @property fm FileManagerクラス
+ * @property logFile ログファイル名
+ * @property logPage ログの内容を含むPageクラス
+ * @property currentBlock 現在のBlockIdクラス
+ * @property latestLSN 最新のログレコードの識別子
+ * @property lastSavedLSN 最後に保存されたログレコードの識別子
  */
 class LogManager(
     private val fm: FileManager,
@@ -17,6 +25,10 @@ class LogManager(
     private var latestLSN = 0
     private var lastSavedLSN = 0
 
+    /**
+     * ログファイルが空の場合は新しい空のブロックを割り当てる
+     * 空ではない場合はlogPageにログファイルの最後のディスクの内容を読み取る
+     */
     init {
         val b = ByteArray(fm.blockSize)
         logPage = Page(b)
@@ -30,17 +42,30 @@ class LogManager(
         }
     }
 
+    /**
+     * 指定したログレコードの識別子[lsn]が最後に保存したログレコードより大きい場合に
+     * 現状のlogPageの内容をディスクに保存する
+     */
     fun flush(lsn: Int) {
         if (lsn >= lastSavedLSN) {
             flush()
         }
     }
 
+    /**
+     * LogManagerのClientがログの内容を読み取る
+     * flush()でメモリの内容をディスクに保存後、ディスク内のログの内容を持つIteratorを返す
+     * @return ディスク内のログの内容を持つIterator
+     */
     fun iterator(): Iterator<ByteArray> {
         flush()
         return LogIterator(fm, currentBlock)
     }
 
+    /**
+     * バイト配列の[logRecord]をログファイルに保存する
+     * @return 最新のログレコードの識別子
+     */
     @Synchronized
     fun append(logRecord: ByteArray): Int {
         var boundary = logPage.getInt(0)
@@ -58,6 +83,10 @@ class LogManager(
         return latestLSN
     }
 
+    /**
+     *
+     * @return 
+     */
     private fun appendNewBlock(): BlockId {
         val block = fm.append(logFile)
         logPage.setInt(0, fm.blockSize)
