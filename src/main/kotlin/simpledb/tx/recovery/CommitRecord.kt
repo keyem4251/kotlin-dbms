de5a1c9872ffa2906f143fdf52fe468afd25171d
keyem4251 <kisk.emura@gmail.com>
2021-11-23 15:55:16 +0900

    implement commit record

---
+++ src/main/kotlin/simpledb/tx/recovery/CommitRecord.kt
@@ -0,0 +1,37 @@
+package simpledb.tx.recovery
+
+import simpledb.file.Page
+import simpledb.log.LogManager
+
+class CommitRecord(private val page: Page): LogRecord {
+    private val transactionNumber: Int
+
+    init {
+        val transactionPosition = Integer.BYTES
+        transactionNumber = page.getInt(transactionPosition)
+    }
+
+    override fun op(): Int {
+        return Operator.COMMIT.id
+    }
+
+    override fun txNumber(): Int {
+        return transactionNumber
+    }
+
+    override fun undo(transaction: Transaction) {}
+
+    override fun toString(): String {
+        return "<COMMIT $transactionNumber>"
+    }
+
+    companion object {
+        fun writeToLog(logManager: LogManager, transactionNumber: Int): Int {
+            val record = ByteArray(2 * Integer.BYTES)
+            val page = Page(record)
+            page.setInt(0, Operator.COMMIT.id)
+            page.setInt(Integer.BYTES, transactionNumber)
+            return logManager.append(record)
+        }
+    }
+}
