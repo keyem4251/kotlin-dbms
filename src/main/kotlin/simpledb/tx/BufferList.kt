4db69b84dfe2e3e58df8bd98a5b4022864d0605b
keyem4251 <kisk.emura@gmail.com>
2021-11-24 21:49:40 +0900

    implement buffer list

--- src/main/kotlin/simpledb/tx/BufferList.kt
+++ src/main/kotlin/simpledb/tx/BufferList.kt
@@ -4,11 +4,35 @@ import simpledb.buffer.Buffer
 import simpledb.buffer.BufferManager
 import simpledb.file.BlockId
 
-class BufferList(val bufferManager: BufferManager) {
+class BufferList(private val bufferManager: BufferManager) {
     private val buffers = mutableMapOf<BlockId, Buffer>()
     private val pins = mutableListOf<BlockId>()
 
-    fun getBuffer(blockId: BlockId): Buffer {
-        
+    fun getBuffer(blockId: BlockId): Buffer? {
+        return buffers[blockId]
+    }
+
+    fun pin(blockId: BlockId) {
+        val buffer = bufferManager.pin(blockId)
+        buffers[blockId] = buffer
+        pins.add(blockId)
+    }
+
+    fun unpin(blockId: BlockId) {
+        val buffer = buffers[blockId]
+        if (buffer != null) {
+            bufferManager.unpin(buffer)
+            pins.remove(blockId)
+            if (!pins.contains(blockId)) buffers.remove(blockId)
+        }
+    }
+
+    fun unpinAll() {
+        for (blockId in pins) {
+            val buffer = buffers[blockId]
+            if (buffer != null) bufferManager.unpin(buffer)
+        }
+        buffers.clear()
+        pins.clear()
     }
 }
