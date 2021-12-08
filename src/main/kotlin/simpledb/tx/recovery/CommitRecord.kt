1605b3fc1053e6f2610f50db2570554119b36453
keyem4251 <kisk.emura@gmail.com>
2021-12-08 20:27:52 +0900

    comment commit record

--- src/main/kotlin/simpledb/tx/recovery/CommitRecord.kt
+++ src/main/kotlin/simpledb/tx/recovery/CommitRecord.kt
@@ -4,6 +4,9 @@ import simpledb.file.Page
 import simpledb.log.LogManager
 import simpledb.tx.Transaction
 
+/**
+ * Commit Log Record
+ */
 class CommitRecord(private val page: Page): LogRecord {
     private val transactionNumber: Int
 
@@ -20,12 +23,19 @@ class CommitRecord(private val page: Page): LogRecord {
         return transactionNumber
     }
 
+    /**
+     * commit recordはやり直しを行う情報は持たない
+     */
     override fun undo(transaction: Transaction) {}
 
     override fun toString(): String {
         return "<COMMIT $transactionNumber>"
     }
 
+    /**
+     * ログにCommit recordを書くメソッド
+     * このログレコードはCOMMIT Operatorの後にトランザクションIDが含まれている。
+     */
     companion object {
         fun writeToLog(logManager: LogManager, transactionNumber: Int): Int {
             val record = ByteArray(2 * Integer.BYTES)
