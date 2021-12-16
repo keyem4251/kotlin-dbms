bb4f48e2ba9aa49f450be29423e37e157ecaca0c
keyem4251 <kisk.emura@gmail.com>
2021-12-16 21:45:32 +0900

    implement record page

--- src/main/kotlin/simpledb/record/RecordPage.kt
+++ src/main/kotlin/simpledb/record/RecordPage.kt
@@ -45,6 +45,54 @@ class RecordPage(
         setFlag(slot, RecordPageState.EMPTY.id)
     }
 
+    fun format() {
+        var slot = 0
+        while (isValidSlot(slot)) {
+            transaction.setInt(blockId, offset(slot), RecordPageState.EMPTY.id, false)
+            val schema = layout.schema()
+            for (folderName in schema.fields) {
+                val layoutOffset = layout.offset(folderName) ?: throw RecordPageException()
+                val folderPosition = offset(slot) + layoutOffset
+                if (schema.type(folderName) == java.sql.Types.INTEGER) {
+                    transaction.setInt(blockId, folderPosition, 0, false)
+                } else {
+                    transaction.setString(blockId, folderPosition, "", false)
+                }
+            }
+            slot++
+        }
+    }
+
+    fun nextAfter(slot: Int): Int {
+        return searchAfter(slot, RecordPageState.USED.id)
+    }
+
+    fun insertAfter(slot: Int): Int {
+        val newSlot = searchAfter(slot, RecordPageState.EMPTY.id)
+        if (newSlot >= 0) setFlag(newSlot, RecordPageState.USED.id)
+        return newSlot
+    }
+
+    fun blockId(): BlockId {
+        return blockId
+    }
+
+    private fun searchAfter(slot: Int, flag: Int): Int {
+        var nextSlot = slot + 1
+        while (isValidSlot(nextSlot)) {
+            val transactionInt = transaction.getInt(blockId, offset(nextSlot))
+            if (transactionInt != null && transactionInt == flag) {
+                return nextSlot
+            }
+            nextSlot++
+        }
+        return -1
+    }
+
+    private fun isValidSlot(slot: Int): Boolean {
+        return offset(slot+1) <= transaction.blockSize()
+    }
+
     // Private auxiliary methods
     private fun setFlag(slot: Int, flag: Int) {
         transaction.setInt(blockId, offset(slot), flag, true)
