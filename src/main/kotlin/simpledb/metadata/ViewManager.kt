763e9c1418abc3d8a219f37066c30f0d48980601
keyem4251 <kisk.emura@gmail.com>
2021-12-24 21:12:50 +0900

    view manager

---
+++ src/main/kotlin/simpledb/metadata/ViewManager.kt
@@ -0,0 +1,44 @@
+package simpledb.metadata
+
+import simpledb.record.Schema
+import simpledb.record.TableScan
+import simpledb.tx.Transaction
+
+class ViewManager(
+    private val isNew: Boolean,
+    val tableManager: TableManager,
+    private val transaction: Transaction,
+) {
+    private val max_viewdef = 100
+
+    init {
+        if (isNew) {
+            val schema = Schema()
+            schema.addStringField("viewname", tableManager.max_name)
+            schema.addStringField("viewdef", max_viewdef)
+            tableManager.createTable("viewcatalog", schema, transaction)
+        }
+    }
+
+    fun createView(viewName: String, viewDef: String, transaction: Transaction) {
+        val layout = tableManager.getLayout("viewcatalog", transaction)
+        val tableScan = TableScan(transaction, "viewcatalog", layout)
+        tableScan.setString("viewname", viewName)
+        tableScan.setString("viewdef", viewDef)
+        tableScan.close()
+    }
+
+    fun getViewDef(viewName: String, transaction: Transaction): String? {
+        var result: String? = null
+        val layout = tableManager.getLayout("viewcatalog", transaction)
+        val tableScan = TableScan(transaction, "viewcatalog", layout)
+        while (tableScan.next()) {
+            if (tableScan.getString("viewname") == viewName) {
+                result = tableScan.getString("viewdef")
+                break
+            }
+        }
+        tableScan.close()
+        return result
+    }
+}
