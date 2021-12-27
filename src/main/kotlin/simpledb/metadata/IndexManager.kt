244caef3aca3850f0eb78bd244554ce2f6bd1c39
keyem4251 <kisk.emura@gmail.com>
2021-12-27 22:13:14 +0900

    implement index manager

--- src/main/kotlin/simpledb/metadata/IndexManager.kt
+++ src/main/kotlin/simpledb/metadata/IndexManager.kt
@@ -1,4 +1,52 @@
 package simpledb.metadata
 
-class IndexManager {
+import simpledb.record.Layout
+import simpledb.record.Schema
+import simpledb.record.TableScan
+import simpledb.tx.Transaction
+
+class IndexManager(
+    private val isNew: Boolean,
+    private val tableManager: TableManager,
+    private val statisticsManager: StatisticsManager,
+    private val transaction: Transaction,
+) {
+    private var layout: Layout
+
+    init {
+        if (isNew) {
+            val schema = Schema()
+            schema.addStringField("indexname", MAX_NAME)
+            schema.addStringField("tablename", MAX_NAME)
+            schema.addStringField("fieldname", MAX_NAME)
+            tableManager.createTable("indexcatalog", schema, transaction)
+        }
+        layout = tableManager.getLayout("indexcatalog", transaction)
+    }
+
+    fun createIndex(indexName: String, tableName: String, fieldName: String, tx: Transaction) {
+        val tableScan = TableScan(tx, "indexcatalog", layout)
+        tableScan.insert()
+        tableScan.setString("indexname", indexName)
+        tableScan.setString("tablename", tableName)
+        tableScan.setString("fieldname", fieldName)
+        tableScan.close()
+    }
+
+    fun getIndexInfo(tableName: String, tx: Transaction): MutableMap<String, IndexInfo> {
+        val result = mutableMapOf<String, IndexInfo>()
+        val tableScan = TableScan(tx, "indexcatalog", layout)
+        while (tableScan.next()) {
+            if (tableScan.getString("tablename") == tableName) {
+                val indexName = tableScan.getString("indexname")
+                val fieldName = tableScan.getString("fieldname")
+                val tableLayout = tableManager.getLayout(tableName, tx)
+                val tableStatisticsInformation = statisticsManager.getStatisticsInformation(tableName, tableLayout, tx)
+                val indexInfo = IndexInfo(indexName, fieldName, tx, tableLayout.schema(), tableStatisticsInformation)
+                result[fieldName] = indexInfo
+            }
+        }
+        tableScan.close()
+        return result
+    }
 }
