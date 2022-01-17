01ac16b54896622fc9800d0efff0f0f661ea714d
keyem4251 <kisk.emura@gmail.com>
2022-01-17 21:23:57 +0900

    implement select scan class

--- src/main/kotlin/simpledb/query/SelectScan.kt
+++ src/main/kotlin/simpledb/query/SelectScan.kt
@@ -1,8 +1,69 @@
 package simpledb.query
 
+import simpledb.record.RID
+
 class SelectScan(
     private val scan: Scan,
-    private val predicate: 
-) {
+    private val predicate: Predicate,
+) :UpdateScan {
+    override fun beforeFirst() {
+        scan.beforeFirst()
+    }
 
+    override fun next(): Boolean {
+        while (scan.next()) {
+            if (predicate.isSatisfied(scan)) return true
+        }
+        return false
+    }
+
+    override fun getInt(fieldName: String): Int {
+        return scan.getInt(fieldName)
+    }
+
+    override fun getString(fieldName: String): String {
+        return scan.getString(fieldName)
+    }
+
+    override fun getVal(fieldName: String): Constant {
+        return scan.getVal(fieldName)
+    }
+
+    override fun hasField(fieldName: String): Boolean {
+        return scan.hasField(fieldName)
+    }
+
+    override fun close() {
+        scan.close()
+    }
+
+    override fun setInt(fieldName: String, value: Int) {
+        val updateScan = scan as UpdateScan
+        updateScan.setInt(fieldName, value)
+    }
+
+    override fun setString(fieldName: String, value: String) {
+        val updateScan = scan as UpdateScan
+        updateScan.setString(fieldName, value)
+    }
+
+    override fun delete() {
+        val updateScan = scan as UpdateScan
+        updateScan.delete()
+    }
+
+    override fun insert() {
+        val updateScan = scan as UpdateScan
+        updateScan.insert()
+    }
+
+    override fun getRid(): RID {
+        val updateScan = scan as UpdateScan
+        return updateScan.getRid()
+    }
+
+    override fun moveToRid(rid: RID) {
+        val updateScan = scan as UpdateScan
+        updateScan.moveToRid(rid)
+    }
 }
