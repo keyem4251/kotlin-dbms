3f204e3b251a9aefe36d9e02fd356213bfd45ebc
keyem4251 <kisk.emura@gmail.com>
2022-01-30 21:27:16 +0900

    implement query data class

--- src/main/kotlin/simpledb/parse/QueryData.kt
+++ src/main/kotlin/simpledb/parse/QueryData.kt
@@ -1,4 +1,27 @@
 package simpledb.parse
 
-class QueryData {
+import simpledb.query.Predicate
+
+class QueryData(
+    private val fields: List<String>,
+    private val tables: Collection<String>,
+    private val predicate: Predicate,
+) {
+    override fun toString(): String {
+        var result = "select "
+        for (filedName in fields) {
+            result += "$filedName, "
+        }
+        result = result.substring(0, result.length-2) //zap final comma
+        result += " from "
+        for (tableName in tables) {
+            result += "$tableName, "
+        }
+        result = result.substring(0, result.length-2) // zap final comma
+        val predicateString = predicate.toString()
+        if (predicateString != "") {
+            result += " where $predicateString"
+        }
+        return result
+    }
 }
