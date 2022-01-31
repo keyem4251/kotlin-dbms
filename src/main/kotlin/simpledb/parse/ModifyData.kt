d2876774dab57b46ee1b27dc3eee1206608b72d9
keyem4251 <kisk.emura@gmail.com>
2022-01-31 21:19:11 +0900

    implement modify data class

--- src/main/kotlin/simpledb/parse/ModifyData.kt
+++ src/main/kotlin/simpledb/parse/ModifyData.kt
@@ -1,4 +1,11 @@
 package simpledb.parse
 
-class ModifyData {
-}
+import simpledb.query.Expression
+import simpledb.query.Predicate
+
+class ModifyData(
+    private val tableName: String,
+    private val fieldName: String,
+    private val newValue: Expression,
+    private val predicate: Predicate,
+)
