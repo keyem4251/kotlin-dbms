f5dad58310916671962201d476ce8ff12a802a0b
keyem4251 <kisk.emura@gmail.com>
2022-01-31 21:20:50 +0900

    implement create view data class

--- src/main/kotlin/simpledb/parse/CreateViewData.kt
+++ src/main/kotlin/simpledb/parse/CreateViewData.kt
@@ -1,4 +1,10 @@
 package simpledb.parse
 
-class CreateViewData {
+class CreateViewData(
+    private val viewName: String,
+    private val queryData: QueryData,
+) {
+    fun viewDef(): String {
+        return queryData.toString()
+    }
 }
