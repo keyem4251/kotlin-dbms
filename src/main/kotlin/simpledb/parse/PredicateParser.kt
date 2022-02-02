8dbd448c810497f07d21afe42d8411a6b3d3d371
keyem4251 <kisk.emura@gmail.com>
2022-02-02 20:46:41 +0900

    implement predicate parser

--- src/main/kotlin/simpledb/parse/PredicateParser.kt
+++ src/main/kotlin/simpledb/parse/PredicateParser.kt
@@ -1,4 +1,39 @@
 package simpledb.parse
 
-class PredicateParser {
+class PredicateParser(private val string: String) {
+    private var lexer: Lexer = Lexer(string)
+
+    fun field() {
+        lexer.eatId()
+    }
+
+    fun constant() {
+        if (lexer.matchStringConstant()) {
+            lexer.eatStringConstant()
+        } else {
+            lexer.eatIntConstant()
+        }
+    }
+
+    fun expression() {
+        if (lexer.matchId()) {
+            field()
+        } else {
+            constant()
+        }
+    }
+
+    fun term() {
+        expression()
+        lexer.eatDelimiter('=')
+        expression()
+    }
+
+    fun predicate() {
+        term()
+        if (lexer.matchKeyword("and")) {
+            lexer.eatKeyword("and")
+            predicate()
+        }
+    }
 }
