11ae574dde9c51c954226f4fda7f5215a5fac56f
keyem4251 <kisk.emura@gmail.com>
2022-02-04 20:45:59 +0900

    comment predicate parser summary and methods

--- src/main/kotlin/simpledb/parse/PredicateParser.kt
+++ src/main/kotlin/simpledb/parse/PredicateParser.kt
@@ -1,12 +1,22 @@
 package simpledb.parse
 
+/**
+ * 条件式と値を扱う5種類のメソッドをもつクラス
+ */
 class PredicateParser(private val string: String) {
     private val lexer: Lexer = Lexer(string)
 
+    /**
+     * フィールド名を解析する
+     */
     fun field() {
         lexer.eatId()
     }
 
+    /**
+     * フィールドの値を解析する
+     * 検索した条件式の中の行の値
+     */
     fun constant() {
         if (lexer.matchStringConstant()) {
             lexer.eatStringConstant()
@@ -15,6 +25,10 @@ class PredicateParser(private val string: String) {
         }
     }
 
+    /**
+     * テーブルの列と値を解析する
+     * 「ID = 1」の「ID」という列名なのか、「1」という値なのか
+     */
     fun expression() {
         if (lexer.matchId()) {
             field()
@@ -23,12 +37,19 @@ class PredicateParser(private val string: String) {
         }
     }
 
+    /**
+     * 条件式の中のそれぞれの項を解析する
+     * 「ID = 1」の「=」を除いた左右の値（ID、1）
+     */
     fun term() {
         expression()
         lexer.eatDelimiter('=')
         expression()
     }
 
+    /**
+     * 条件式を解析する
+     */
     fun predicate() {
         term()
         if (lexer.matchKeyword("and")) {
