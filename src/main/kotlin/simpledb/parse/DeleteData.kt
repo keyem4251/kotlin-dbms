46acef8f77c5af65fd2c4fb196eb5d4a304d74d8
keyem4251 <kisk.emura@gmail.com>
2022-02-05 11:16:16 +0900

    comment delete data class

--- src/main/kotlin/simpledb/parse/DeleteData.kt
+++ src/main/kotlin/simpledb/parse/DeleteData.kt
@@ -2,6 +2,10 @@ package simpledb.parse
 
 import simpledb.query.Predicate
 
+/**
+ * Deleteを行うのに必要な値を格納するクラス
+ * <Delete> := DELETE FROM IdToken [ WHERE <Predicate> ]
+ */
 data class DeleteData(
     private val tableName: String,
     private val predicate: Predicate,
