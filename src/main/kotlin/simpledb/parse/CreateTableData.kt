12d18de492b50a4934c5146c5c9232e19a3aa73a
keyem4251 <kisk.emura@gmail.com>
2022-02-05 11:18:00 +0900

    comment create table data class

--- src/main/kotlin/simpledb/parse/CreateTableData.kt
+++ src/main/kotlin/simpledb/parse/CreateTableData.kt
@@ -2,6 +2,10 @@ package simpledb.parse
 
 import simpledb.record.Schema
 
+/**
+ * テーブルを作成するのに必要な値を格納するクラス
+ * <CreateTable> := CREATE TABLE IdToken ( <FieldDefs> )
+ */
 data class CreateTableData(
     private val tableName: String,
     private val schema: Schema,
