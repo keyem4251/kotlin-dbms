bfaa9a482f8159ed035e225b1d74e51e1e128647
keyem4251 <kisk.emura@gmail.com>
2022-02-05 11:20:20 +0900

    comment create view data class

--- src/main/kotlin/simpledb/parse/CreateViewData.kt
+++ src/main/kotlin/simpledb/parse/CreateViewData.kt
@@ -1,5 +1,9 @@
 package simpledb.parse
 
+/**
+ * Viewを作成するのに必要な値を格納するクラス
+ * <CreateView> := CREATE VIEW IdToken AS <Query>
+ */
 class CreateViewData(
     private val viewName: String,
     private val queryData: QueryData,
