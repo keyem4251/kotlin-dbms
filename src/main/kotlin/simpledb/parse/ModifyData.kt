b817c8db0142817c4df0c3f15151e0f443a27220
keyem4251 <kisk.emura@gmail.com>
2022-02-05 11:16:57 +0900

    comment modify data class

--- src/main/kotlin/simpledb/parse/ModifyData.kt
+++ src/main/kotlin/simpledb/parse/ModifyData.kt
@@ -3,6 +3,10 @@ package simpledb.parse
 import simpledb.query.Expression
 import simpledb.query.Predicate
 
+/**
+ * Updateを行うのに必要な値を格納するクラス
+ * <Modify> := UPDATE IdToken SET <Field> = <Expression> [ WHERE <Predicate> ]
+ */
 class ModifyData(
     private val tableName: String,
     private val fieldName: String,
