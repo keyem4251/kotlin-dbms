7c01bfaebd778685c805eda9d90182340750bd89
keyem4251 <kisk.emura@gmail.com>
2022-02-05 11:15:32 +0900

    comment insert data class

--- src/main/kotlin/simpledb/parse/InsertData.kt
+++ src/main/kotlin/simpledb/parse/InsertData.kt
@@ -2,6 +2,10 @@ package simpledb.parse
 
 import simpledb.query.Constant
 
+/**
+ * Insertを行うのに必要な値を格納するクラス
+ * <Insert> := INSERT INTO IdToken ( <FieldList> ) VALUES ( <ConstList> )
+ */
 data class InsertData(
     private val tableName: String,
     private val fields: List<String>,
