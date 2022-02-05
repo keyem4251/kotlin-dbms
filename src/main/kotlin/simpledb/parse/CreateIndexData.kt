eab8811c22c13f1f70f77bddbc18dfda9d4f3146
keyem4251 <kisk.emura@gmail.com>
2022-02-05 11:21:06 +0900

    comment create index data class

--- src/main/kotlin/simpledb/parse/CreateIndexData.kt
+++ src/main/kotlin/simpledb/parse/CreateIndexData.kt
@@ -1,5 +1,9 @@
 package simpledb.parse
 
+/**
+ * Indexを作成するのに必要な値を格納するクラス
+ * <CreateIndex> := CREATE INDEX IdToken ON IdToken ( <Field> )
+ */
 data class CreateIndexData(
     private val indexName: String,
     private val tableName: String,
