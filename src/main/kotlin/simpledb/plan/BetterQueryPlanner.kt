0c3eb34a2c1af514d60e858f7816edb96f9b9bed
keyem4251 <kisk.emura@gmail.com>
2022-02-06 18:20:33 +0900

    implmenet better query planner

---
+++ src/main/kotlin/simpledb/plan/BetterQueryPlanner.kt
@@ -0,0 +1,46 @@
+package simpledb.plan
+
+import simpledb.metadata.MetadataManager
+import simpledb.parse.Parser
+import simpledb.parse.QueryData
+import simpledb.tx.Transaction
+
+class BetterQueryPlanner(
+    private val metadataManager: MetadataManager,
+) {
+    fun createPlan(data: QueryData, transaction: Transaction): Plan {
+        // Step 1: Create a plan for each mentioned table or view.
+        val plans = mutableListOf<Plan>()
+        for (tableName in data.tables) {
+            val viewdef = metadataManager.getViewDef(tableName, transaction)
+            if (viewdef != null) {
+                // Recursively plan the view.
+                val parser = Parser(viewdef)
+                val viewData = parser.query()
+                plans.add(createPlan(viewData, transaction))
+            } else {
+                plans.add(TablePlan(transaction, tableName, metadataManager))
+            }
+        }
+
+        // Step 2: Create the product of all table plans
+        var plan = plans.removeAt(0)
+        for (nextPlan in plans) {
+            // Try both orderings and choose the one having lowest cost
+            val choice1: Plan = ProductPlan(nextPlan, plan)
+            val choice2: Plan = ProductPlan(plan, nextPlan)
+            plan = if (choice1.blocksAccessed() < choice2.blocksAccessed()) {
+                choice1
+            } else {
+                choice2
+            }
+        }
+
+        // Step 3: Add a selection plan for the predicate
+        plan = SelectPlan(plan, data.predicate)
+
+        // Step 4: Project on the field names
+        plan = ProjectPlan(plan, data.fields)
+        return plan
+    }
+}
