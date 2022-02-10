021130afcc6288654e925c1a7d719acb8b5a86d3
keyem4251 <kisk.emura@gmail.com>
2022-02-10 20:49:56 +0900

    comment project plan class

--- src/main/kotlin/simpledb/plan/ProjectPlan.kt
+++ src/main/kotlin/simpledb/plan/ProjectPlan.kt
@@ -4,6 +4,9 @@ import simpledb.query.ProjectScan
 import simpledb.query.Scan
 import simpledb.record.Schema
 
+/**
+ * 受け取ったplanで扱われているテーブルから、指定された[fieldList]フィールドを取り出すコストを見積もるクラス
+ */
 class ProjectPlan(
     private val plan: Plan,
     private val fieldList: List<String>,
@@ -11,7 +14,7 @@ class ProjectPlan(
     private val schema = Schema()
 
     /**
-     * 受け取ったplanのスキーマから自身のスキーマを作成する
+     * 受け取ったplanのスキーマ、フィールド名からから自身のスキーマを作成する
      */
     init {
         for (fieldName in fieldList) {
@@ -19,23 +22,38 @@ class ProjectPlan(
         }
     }
 
+    /**
+     * テーブルのフィールドを返すためのProjectScanクラスを返す
+     */
     override fun open(): Scan {
         val scan = plan.open()
         return ProjectScan(scan, schema.fields)
     }
 
+    /**
+     * テーブルのフィールドを返すためのブロックアクセスの回数を推定する
+     */
     override fun blocksAccessed(): Int {
         return plan.blocksAccessed()
     }
 
+    /**
+     * テーブルのフィールドを返すための行の数を推定する
+     */
     override fun recordsOutput(): Int {
         return plan.recordsOutput()
     }
 
+    /**
+     * テーブルのフィールドを返す場合の[fieldName]指定されたフィールド名のばらつきをを推定する
+     */
     override fun distinctValues(fieldName: String): Int {
         return plan.distinctValues(fieldName)
     }
 
+    /**
+     * 自身のスキーマを返す
+     */
     override fun schema(): Schema {
         return schema
     }
