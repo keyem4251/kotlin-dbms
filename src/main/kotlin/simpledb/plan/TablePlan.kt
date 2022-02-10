0ceeda85a582e2a317009facc49b2e398553af8f
keyem4251 <kisk.emura@gmail.com>
2022-02-10 20:29:40 +0900

    comment table plan class

--- src/main/kotlin/simpledb/plan/TablePlan.kt
+++ src/main/kotlin/simpledb/plan/TablePlan.kt
@@ -10,6 +10,8 @@ import simpledb.tx.Transaction
 
 /**
  * 保存されたテーブルのコストをテーブルのメタデータマネージャを用いて推定する
+ *
+ * @property tableName クエリの中の特定のテーブル
  */
 class TablePlan(
     private val transaction: Transaction,
@@ -20,22 +22,37 @@ class TablePlan(
     private var statisticsInformation: StatisticsInformation =
         metadataManager.getStatisticsInformation(tableName, layout, transaction)
 
+    /**
+     * テーブルを読み取るためのTableScanクラスを返す
+     */
     override fun open(): Scan {
         return TableScan(transaction, tableName, layout)
     }
 
+    /**
+     * テーブルの統計情報からブロックのアクセスの回数を推定する
+     */
     override fun blocksAccessed(): Int {
         return statisticsInformation.blockAccessed()
     }
 
+    /**
+     * テーブルの統計情報からテーブルのレコード数を推定する
+     */
     override fun recordsOutput(): Int {
         return statisticsInformation.recordsOutput()
     }
 
+    /**
+     * テーブルの統計情報から指定されたフィールド名の数を推定する
+     */
     override fun distinctValues(fieldName: String): Int {
         return statisticsInformation.distinctValues(fieldName)
     }
 
+    /**
+     * テーブルのスキーマを返す
+     */
     override fun schema(): Schema {
         return layout.schema()
     }
