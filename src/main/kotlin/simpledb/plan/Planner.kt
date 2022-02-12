5eb9f539d0b1804202376f50db8bc74fa5be574f
keyem4251 <kisk.emura@gmail.com>
2022-02-12 20:46:27 +0900

    comment planner class

--- src/main/kotlin/simpledb/plan/Planner.kt
+++ src/main/kotlin/simpledb/plan/Planner.kt
@@ -3,10 +3,18 @@ package simpledb.plan
 import simpledb.parse.*
 import simpledb.tx.Transaction
 
+/**
+ * SimpleDBがSQLのコストを取得するためのプランナークラス
+ * 参照系、更新系のプランをそれぞれのメソッドで返す
+ */
 class Planner(
     private val queryPlanner: QueryPlanner,
     private val updatePlanner: UpdatePlanner,
 ) {
+    /**
+     * 参照系のSQLに対してプランを返す
+     * parserでSQLを解釈し、queryDataとしてSQLをデータに格納しプランを返す
+     */
     fun createQueryPlan(cmd: String, transaction: Transaction): Plan {
         val parser = Parser(cmd)
         val queryData = parser.query()
@@ -14,27 +22,31 @@ class Planner(
         return queryPlanner.createPlan(queryData, transaction)
     }
 
+    /**
+     * 更新系のSQLに対してプランを返す
+     * parserでSQLを解釈し、更新系の情報をupdateDateとして該当するプランを返す
+     */
     fun executeUpdate(cmd: String, transaction: Transaction): Int {
         val parser = Parser(cmd)
         // code to verify the update command should be here...
-        return when (val any = parser.updateCmd()) {
+        return when (val updateData = parser.updateCmd()) {
             is InsertData -> {
-                updatePlanner.executeInsert(any, transaction)
+                updatePlanner.executeInsert(updateData, transaction)
             }
             is DeleteData -> {
-                updatePlanner.executeDelete(any, transaction)
+                updatePlanner.executeDelete(updateData, transaction)
             }
             is ModifyData -> {
-                updatePlanner.executeModify(any, transaction)
+                updatePlanner.executeModify(updateData, transaction)
             }
             is CreateTableData -> {
-                updatePlanner.executeCreateTable(any, transaction)
+                updatePlanner.executeCreateTable(updateData, transaction)
             }
             is CreateViewData -> {
-                updatePlanner.executeCreateView(any, transaction)
+                updatePlanner.executeCreateView(updateData, transaction)
             }
             is CreateIndexData -> {
-                updatePlanner.executeCreateIndex(any, transaction)
+                updatePlanner.executeCreateIndex(updateData, transaction)
             }
             else -> {
                 0
