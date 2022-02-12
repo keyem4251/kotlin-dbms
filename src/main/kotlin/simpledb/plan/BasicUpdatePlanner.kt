082d1052dd7b549708398c36801d636be1d6bc05
keyem4251 <kisk.emura@gmail.com>
2022-02-12 20:28:42 +0900

    comment basic update planner

--- src/main/kotlin/simpledb/plan/BasicUpdatePlanner.kt
+++ src/main/kotlin/simpledb/plan/BasicUpdatePlanner.kt
@@ -11,6 +11,10 @@ import simpledb.tx.Transaction
 class BasicUpdatePlanner(
     private val metadataManager: MetadataManager,
 ) : UpdatePlanner {
+
+    /**
+     * SelectPlanを呼び条件式を評価、コストを推定し、UpdateScanでテーブルの行を削除していく
+     */
     override fun executeDelete(data: DeleteData, transaction: Transaction): Int {
         var plan: Plan = TablePlan(transaction, data.tableName, metadataManager)
         plan = SelectPlan(plan, data.predicate)
@@ -24,6 +28,9 @@ class BasicUpdatePlanner(
         return count
     }
 
+    /**
+     * SelectPlanを呼び条件式を評価、コストを推定し、UpdateScanでテーブルの行を更新していく
+     */
     override fun executeModify(data: ModifyData, transaction: Transaction): Int {
         var plan: Plan = TablePlan(transaction, data.tableName, metadataManager)
         plan = SelectPlan(plan, data.predicate)
@@ -38,6 +45,9 @@ class BasicUpdatePlanner(
         return count
     }
 
+    /**
+     * SelectPlanを呼び条件式を評価、コストを推定し、UpdateScanでテーブルの行を挿入していく
+     */
     override fun executeInsert(data: InsertData, transaction: Transaction): Int {
         val plan: Plan = TablePlan(transaction, data.tableName, metadataManager)
         val updateScan = plan.open() as UpdateScan
@@ -51,16 +61,25 @@ class BasicUpdatePlanner(
         return 1
     }
 
+    /**
+     * レコードを操作しないためScanクラスは呼ばずに、メタデータを操作しテーブルを作成する
+     */
     override fun executeCreateTable(data: CreateTableData, transaction: Transaction): Int {
         metadataManager.createTable(data.tableName, data.schema, transaction)
         return 0
     }
 
+    /**
+     * レコードを操作しないためScanクラスは呼ばずに、メタデータを操作しビューを作成する
+     */
     override fun executeCreateView(data: CreateViewData, transaction: Transaction): Int {
         metadataManager.createView(data.viewName, data.viewDef(), transaction)
         return 0
     }
 
+    /**
+     * レコードを操作しないためScanクラスは呼ばずに、メタデータを操作しインデックスを作成する
+     */
     override fun executeCreateIndex(data: CreateIndexData, transaction: Transaction): Int {
         metadataManager.createIndex(data.indexName, data.tableName, data.fieldName, transaction)
         return 0
