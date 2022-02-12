17a24dd6b588064944f7ba8da57f5714e9480b78
keyem4251 <kisk.emura@gmail.com>
2022-02-12 20:33:32 +0900

    comment query planner interface

--- src/main/kotlin/simpledb/plan/QueryPlanner.kt
+++ src/main/kotlin/simpledb/plan/QueryPlanner.kt
@@ -3,6 +3,9 @@ package simpledb.plan
 import simpledb.parse.QueryData
 import simpledb.tx.Transaction
 
+/**
+ * 参照系のクエリプランナーのインターフェイス
+ */
 interface QueryPlanner {
     fun createPlan(data: QueryData, transaction: Transaction): Plan
 }
