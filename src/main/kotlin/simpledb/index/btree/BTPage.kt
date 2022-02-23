d0ed958ab83ada642d8b8249bbcb4e7e469da2bc
keyem4251 <kisk.emura@gmail.com>
2022-02-23 20:51:16 +0900

    comment bt page class

--- src/main/kotlin/simpledb/index/btree/BTPage.kt
+++ src/main/kotlin/simpledb/index/btree/BTPage.kt
@@ -6,6 +6,14 @@ import simpledb.record.Layout
 import simpledb.record.RID
 import simpledb.tx.Transaction
 
+/**
+ * B TreeのためのPage（Btree Node）を持つ
+ * directoryとleafが共通で持つ操作を下記の操作をクラス化したもの
+ * - 行をソートされた順序で保持する
+ * - それぞれの行はページ内で必要により移動できるように固定のIDは持たない
+ * - 行を他のページに分割することができる
+ * - それぞれのページはフラグを持つ（Directoryは階層構造を判断する、Leafはブロックのオーバーフローを把握するのに使用する）
+ */
 class BTPage(
     private val transaction: Transaction,
     private var currentBlock: BlockId?,
@@ -15,6 +23,9 @@ class BTPage(
         transaction.pin(currentBlock ?: throw RuntimeException("null error"))
     }
 
+    /**
+     * [searchKey]指定されたキーを最初に持つ行がある位置を計算し、その前の位置を返す
+     */
     fun findSlotBefore(searchKey: Constant): Int {
         var slot = 0
         while (slot < getNumRecords() && getDataVal(slot).compareTo(searchKey) < 0) {
@@ -23,15 +34,26 @@ class BTPage(
         return slot - 1
     }
 
+    /**
+     * バッファを開放しページを閉じる
+     */
     fun close() {
         if (currentBlock != null) transaction.unpin(currentBlock!!)
         currentBlock = null
     }
 
+    /**
+     * ブロックが埋まっていればtrue、埋まっていなければfalseを返す
+     */
     fun isFull(): Boolean {
         return slotPosition(getNumRecords()+1) >= transaction.blockSize()
     }
 
+    /**
+     * [splitPosition]指定された位置でPageを分割する
+     * 新しいPageに[flag]を指定する（directoryは階層構造、leafはブロックの限界値: オーバーフローする値を知るため）
+     * 新しいPageが作り、分割する位置から始まる行を新しいPageに移動させる
+     */
     fun split(splitPosition: Int, flag: Int): BlockId {
         val newBlock = appendNew(flag)
         val newPage = BTPage(transaction, newBlock, layout)
@@ -41,20 +63,32 @@ class BTPage(
         return newBlock
     }
 
+    /**
+     * [slot]指定されたslotにある行の値を返す
+     */
     fun getDataVal(slot: Int): Constant {
         return getVal(slot, "dataval")
     }
 
+    /**
+     * Pageのflagを返す
+     */
     fun getFlag(): Int {
         val nonNullBlock = currentBlock ?: throw RuntimeException("null error")
         return transaction.getInt(nonNullBlock, 0) ?: throw RuntimeException("null error")
     }
 
+    /**
+     * [value]指定された値でflagをPageに設定する
+     */
     fun setFlag(value: Int) {
         val nonNullBlock = currentBlock ?: throw RuntimeException("null error")
         transaction.setInt(nonNullBlock, 0, value, true)
     }
 
+    /**
+     * [flag]指定されたフラグで新しいブロックをB Treeファイルの末尾に追加する
+     */
     fun appendNew(flag: Int): BlockId {
         val nonNullBlock = currentBlock ?: throw RuntimeException("null error")
         val blockId = transaction.append(nonNullBlock.filename)
@@ -86,10 +120,17 @@ class BTPage(
     }
 
     // Methods called only by BTreeDir
+
+    /**
+     * [slot]指定されたスロットに保存されているインデックスの行のブロックのIDを返す
+     */
     fun getChildNum(slot: Int): Int {
         return getInt(slot, "block")
     }
 
+    /**
+     * [slot]指定されたスロットにdirectoryのentry（インデックスの行 [value], [blockNumber]）を挿入する
+     */
     fun insertDir(slot: Int, value: Constant, blockNumber: Int) {
         insert(slot)
         setVal(slot, "dataval", value)
@@ -97,10 +138,17 @@ class BTPage(
     }
 
     // Methods called only by BTreeLeaf
+
+    /**
+     * [slot]指定されたスロットに保存されているleaf indexの行のRIDを返す
+     */
     fun getDataRid(slot: Int): RID {
         return RID(getInt(slot, "block"), getInt(slot, "id"))
     }
 
+    /**
+     * [slot]指定されたスロットにleaf indexの行を挿入する
+     */
     fun insertLeaf(slot: Int, value: Constant, rid: RID) {
         insert(slot)
         setVal(slot, "dataval", value)
@@ -108,6 +156,9 @@ class BTPage(
         setInt(slot, "id", rid.slot)
     }
 
+    /**
+     * [slot]指定されたスロットのleaf indexの行を削除する
+     */
     fun delete(slot: Int) {
         for (i in slot+1 until getNumRecords()) {
             copyRecord(i, i-1)
