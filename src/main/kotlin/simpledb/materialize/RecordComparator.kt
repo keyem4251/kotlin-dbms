f2256765c984c313e9665c5623a980d55370b4f1
keyem4251 <kisk.emura@gmail.com>
2022-03-14 21:31:40 +0900

    comment record comparator

--- src/main/kotlin/simpledb/materialize/RecordComparator.kt
+++ src/main/kotlin/simpledb/materialize/RecordComparator.kt
@@ -2,9 +2,17 @@ package simpledb.materialize
 
 import simpledb.query.Scan
 
+/**
+ * scanのためのcomparator
+ */
 class RecordComparator(
     private val fields: Collection<String>
 ) : Comparator<Scan> {
+    /**
+     * 受け取った2つのscan（[o1], [o2]）の現在のレコードを比較する
+     * レコードが異なる値を持つフィールドを保つ場合に、その値が比較の結果として使用される。
+     * 2つのレコードがすべてのソートフィールドで同じ値を保つ場合には0を返す
+     */
     override fun compare(o1: Scan?, o2: Scan?): Int {
         for (fieldName in fields) {
             val value1 = o1?.getVal(fieldName) ?: throw RuntimeException("null error")
