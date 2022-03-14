d5627ae15a5e8cb0210e6c5e6362a2f2c014446b
keyem4251 <kisk.emura@gmail.com>
2022-03-14 21:53:51 +0900

    comment group by plan

--- src/main/kotlin/simpledb/materialize/GroupByPlan.kt
+++ src/main/kotlin/simpledb/materialize/GroupByPlan.kt
@@ -5,6 +5,13 @@ import simpledb.query.Scan
 import simpledb.record.Schema
 import simpledb.tx.Transaction
 
+/**
+ * 入力となるテーブル、集約を行うフィールド、関数の3つをもとにする
+ *
+ * @property plan 入力レコードのために初期化時にsort planにする
+ * @property groupFields group by を行うフィールド
+ * @property aggregationFns 集約を行う関数
+ */
 class GroupByPlan(
     private val transaction: Transaction,
     private var plan: Plan,
@@ -13,6 +20,9 @@ class GroupByPlan(
 ) : Plan {
     private val schema = Schema()
 
+    /**
+     * 集約を行うフィールドでsortを行う
+     */
     init {
         plan = SortPlan(plan, transaction, groupFields)
         for (fieldName in groupFields) {
@@ -23,15 +33,25 @@ class GroupByPlan(
         }
     }
 
+    /**
+     * Group Byを行うためのscanを作成する
+     */
     override fun open(): Scan {
         val scan: Scan = plan.open()
         return GroupByScan(scan, groupFields, aggregationFns)
     }
 
+    /**
+     * ブロックアクセスの数を推定し返す
+     */
     override fun blocksAccessed(): Int {
         return plan.blocksAccessed()
     }
 
+    /**
+     * グループ化された状態のレコードの数を推定し返す
+     * 各フィールドのばらつきの積を計算する
+     */
     override fun recordsOutput(): Int {
         var numberGroups = 1
         for (fieldName in groupFields) {
@@ -40,6 +60,9 @@ class GroupByPlan(
         return numberGroups
     }
 
+    /**
+     * レコードのばらつきを返す
+     */
     override fun distinctValues(fieldName: String): Int {
         return if (plan.schema().hasField(fieldName)) {
             plan.distinctValues(fieldName)
@@ -48,6 +71,9 @@ class GroupByPlan(
         }
     }
 
+    /**
+     * スキーマを返す（集約するフィールドと集約する関数のフィールドを持つ）
+     */
     override fun schema(): Schema {
         return schema
     }
