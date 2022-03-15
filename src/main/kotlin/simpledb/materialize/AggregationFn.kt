976796883178d352c74fe00245d0454b6263e42e
keyem4251 <kisk.emura@gmail.com>
2022-03-15 21:33:25 +0900

    comment aggregation fn interface

--- src/main/kotlin/simpledb/materialize/AggregationFn.kt
+++ src/main/kotlin/simpledb/materialize/AggregationFn.kt
@@ -3,9 +3,27 @@ package simpledb.materialize
 import simpledb.query.Constant
 import simpledb.query.Scan
 
+/**
+ * 集約を行う関数のインターフェイス
+ */
 interface AggregationFn {
+    /**
+     * 現在のレコードを最初のレコードとして使用し新しいグループを開始する
+     */
     fun processFirst(scan: Scan)
+
+    /**
+     * 既存のグループに次のレコードを追加し、集約を行う
+     */
     fun processNext(scan: Scan)
+
+    /**
+     * 集約を行うフィールド名を返す
+     */
     fun fieldName(): String
+
+    /**
+     * 集約した値を返す
+     */
     fun value(): Constant
 }
