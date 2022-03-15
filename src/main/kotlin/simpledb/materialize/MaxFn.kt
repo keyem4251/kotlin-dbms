59f16545e95cdfa81313669c13b714648690f410
keyem4251 <kisk.emura@gmail.com>
2022-03-15 21:37:16 +0900

    comment max fn

--- src/main/kotlin/simpledb/materialize/MaxFn.kt
+++ src/main/kotlin/simpledb/materialize/MaxFn.kt
@@ -3,15 +3,24 @@ package simpledb.materialize
 import simpledb.query.Constant
 import simpledb.query.Scan
 
+/**
+ * グループの中の最大値を返す関数
+ */
 class MaxFn(
     private val fieldName: String
 ) : AggregationFn {
     private lateinit var value: Constant
 
+    /**
+     * 現在のレコードをグループの最大値として設定
+     */
     override fun processFirst(scan: Scan) {
         value = scan.getVal(fieldName)
     }
 
+    /**
+     * 現在のレコードとグループの最大値として設定されている値を比較し、大きい方を設定する
+     */
     override fun processNext(scan: Scan) {
         val newValue = scan.getVal(fieldName)
         if (newValue.compareTo(value) > 0) {
@@ -19,10 +28,16 @@ class MaxFn(
         }
     }
 
+    /**
+     * フィールド名を返す
+     */
     override fun fieldName(): String {
         return "maxof$fieldName"
     }
 
+    /**
+     * 最大値を返す
+     */
     override fun value(): Constant {
         return value
     }
