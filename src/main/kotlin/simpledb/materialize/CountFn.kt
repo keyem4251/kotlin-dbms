899656a5d0c768db89bb5c1bc0053260f057465d
keyem4251 <kisk.emura@gmail.com>
2022-03-15 21:38:47 +0900

    comment count fn

--- src/main/kotlin/simpledb/materialize/CountFn.kt
+++ src/main/kotlin/simpledb/materialize/CountFn.kt
@@ -3,23 +3,38 @@ package simpledb.materialize
 import simpledb.query.Constant
 import simpledb.query.Scan
 
+/**
+ * グループ内のレコードの数を返す
+ */
 class CountFn(
     private val fieldName: String,
 ) : AggregationFn {
     private var count: Int = 0
 
+    /**
+     * グループの数を1として設定する
+     */
     override fun processFirst(scan: Scan) {
         count = 1
     }
 
+    /**
+     * グループの数に1を足す
+     */
     override fun processNext(scan: Scan) {
         count += 1
     }
 
+    /**
+     * フィールド名を返す
+     */
     override fun fieldName(): String {
         return "countof$fieldName"
     }
 
+    /**
+     * グループ内のレコードの数を返す
+     */
     override fun value(): Constant {
         return Constant(count)
     }
