bc94c23abec6d93dff2999aabd22b05e034d8506
keyem4251 <kisk.emura@gmail.com>
2022-03-16 21:22:48 +0900

    comment mrege join scan

--- src/main/kotlin/simpledb/materialize/MergeJoinScan.kt
+++ src/main/kotlin/simpledb/materialize/MergeJoinScan.kt
@@ -3,6 +3,15 @@ package simpledb.materialize
 import simpledb.query.Constant
 import simpledb.query.Scan
 
+/**
+ * merge joinを行うプラン（indexを用いたjoinではなく）
+ *
+ * @property scan 入力となるテーブル1
+ * @property sortScan 入力となるテーブル2 ソートされている
+ * @property fieldName1 テーブル1の結合を行うフィールド1
+ * @property fieldName2 テーブル2の結合を行うフィールド2
+ * @property joinValue 直前に結合を行った値
+ */
 class MergeJoinScan(
     private val scan: Scan,
     private val sortScan: SortScan,
@@ -15,16 +24,28 @@ class MergeJoinScan(
         beforeFirst()
     }
 
+    /**
+     * それぞれのテーブルの位置を最初のレコードの前にすすめる
+     */
     override fun beforeFirst() {
         scan.beforeFirst()
         sortScan.beforeFirst()
     }
 
+    /**
+     * それぞれのテーブルを閉じる
+     */
     override fun close() {
         scan.close()
         sortScan.close()
     }
 
+    /**
+     * 次のレコードにすすめる
+     * テーブル2が直前に結合を行った値[joinValue]と同じ値を持っている場合はテーブル2をすすめる
+     * そうではなく、テーブル1の次の値が[joinValue]と同じ値の場合はテーブル2を[joinValue]を持つ位置に再移動する
+     * テーブル1、テーブル2がともに[joinValue]と同じ値を持たない場合はそれぞれのテーブルの値が同じになるまで繰り返しテーブルをすすめる
+     */
     override fun next(): Boolean {
         var hasMore2 = sortScan.next()
         if (hasMore2 && sortScan.getVal(fieldName2).equals(joinValue)) return true
@@ -51,6 +72,9 @@ class MergeJoinScan(
         return false
     }
 
+    /**
+     * 指定されたフィールド[fieldName]の値を返す
+     */
     override fun getInt(fieldName: String): Int {
         return if (scan.hasField(fieldName)) {
             scan.getInt(fieldName)
@@ -59,6 +83,9 @@ class MergeJoinScan(
         }
     }
 
+    /**
+     * 指定されたフィールド[fieldName]の値を返す
+     */
     override fun getString(fieldName: String): String {
         return if (scan.hasField(fieldName)) {
             scan.getString(fieldName)
@@ -67,6 +94,9 @@ class MergeJoinScan(
         }
     }
 
+    /**
+     * 指定されたフィールド[fieldName]の値を返す
+     */
     override fun getVal(fieldName: String): Constant {
         return if (scan.hasField(fieldName)) {
             scan.getVal(fieldName)
@@ -75,6 +105,9 @@ class MergeJoinScan(
         }
     }
 
+    /**
+     * 指定されたフィールド[fieldName]を持っているか判定する
+     */
     override fun hasField(fieldName: String): Boolean {
         return scan.hasField(fieldName) || sortScan.hasField(fieldName)
     }
