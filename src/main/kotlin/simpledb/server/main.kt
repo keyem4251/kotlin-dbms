1da4232b2cc7c996ff48e572627e540beb8b7731
keyem4251 <kisk.emura@gmail.com>
2022-03-18 21:01:04 +0900

    comment todo

--- src/main/kotlin/simpledb/server/main.kt
+++ src/main/kotlin/simpledb/server/main.kt
@@ -1,5 +1,20 @@
 package simpledb.server
 
-fun main(args: Array<String>) {
+import java.rmi.registry.LocateRegistry
 
+// TODO simpleijでembdeddriverで書き換え
+fun main(args: Array<String>) {
+    // configure and initialize the database
+    val directoryName = if (args.size === 0) {
+        "studentdb"
+    } else {
+        args[0]
+    }
+    val db = SimpleDB(directoryName)
+
+    // create a registry specific for the server on the default port
+    val registry = LocateRegistry.createRegistry(1099)
+
+    // and post the server entry in it
+    val d = Rem
 }
