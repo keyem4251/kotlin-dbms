3db1f068c2e0a800bbb96c5d57529c575063fd11
keyem4251 <kisk.emura@gmail.com>
2022-03-19 21:45:57 +0900

    implement embedded connection

--- src/main/kotlin/simpledb/jdbc/embdded/EmbeddedConnection.kt
+++ src/main/kotlin/simpledb/jdbc/embdded/EmbeddedConnection.kt
@@ -1,4 +1,30 @@
 package simpledb.jdbc.embdded
 
-class EmbeddedConnection {
+import simpledb.jdbc.ConnectionAdapter
+import simpledb.server.SimpleDB
+import java.sql.Statement
+
+class EmbeddedConnection(
+    private val db: SimpleDB,
+): ConnectionAdapter() {
+    var currentTransaction = db.newTransaction()
+    private val planner = db.planner
+
+    override fun createStatement(resultSetType: Int, resultSetConcurrency: Int, resultSetHoldability: Int): Statement {
+        return EmbeddedStatement(this, planner)
+    }
+
+    override fun close() {
+        commit()
+    }
+
+    override fun commit() {
+        currentTransaction.commit()
+        currentTransaction = db.newTransaction()
+    }
+
+    override fun rollback() {
+        currentTransaction.rollback()
+        currentTransaction = db.newTransaction()
+    }
 }
