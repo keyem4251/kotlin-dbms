56136cbf9fd24e8f65db81f078350d5eb847fa1b
keyem4251 <kisk.emura@gmail.com>
2022-03-19 21:44:53 +0900

    implement embedded statement

--- src/main/kotlin/simpledb/jdbc/embdded/EmbeddedStatement.kt
+++ src/main/kotlin/simpledb/jdbc/embdded/EmbeddedStatement.kt
@@ -1,4 +1,39 @@
 package simpledb.jdbc.embdded
 
-class EmbeddedStatement {
+import simpledb.jdbc.StatementAdapter
+import simpledb.plan.Plan
+import simpledb.plan.Planner
+import java.sql.ResultSet
+import java.sql.SQLException
+
+class EmbeddedStatement(
+    private val connection: EmbeddedConnection,
+    private val planner: Planner,
+) : StatementAdapter() {
+    override fun executeQuery(sql: String?): ResultSet {
+        try {
+            val transaction = connection.currentTransaction
+            val cmd = sql ?: throw RuntimeException("null sql error")
+            val plan: Plan = planner.createQueryPlan(cmd, transaction)
+            return EmbeddedResultSet(plan, connection)
+        } catch (e: RuntimeException) {
+            connection.rollback()
+            throw SQLException(e)
+        }
+    }
+
+    override fun executeUpdate(sql: String?): Int {
+        try {
+            val transaction = connection.currentTransaction
+            val cmd = sql ?: throw RuntimeException("null sql error")
+            val result = planner.executeUpdate(cmd, transaction)
+            connection.commit()
+            return result
+        } catch (e: RuntimeException) {
+            connection.rollback()
+            throw SQLException(e)
+        }
+    }
+
+    override fun close() {}
 }
