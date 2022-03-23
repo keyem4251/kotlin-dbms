9227ceb530a305aef754aa9d7e7a8473225f3553
keyem4251 <kisk.emura@gmail.com>
2022-03-23 23:17:04 +0900

    mod override function

--- src/main/kotlin/simpledb/jdbc/embdded/EmbeddedConnection.kt
+++ src/main/kotlin/simpledb/jdbc/embdded/EmbeddedConnection.kt
@@ -2,16 +2,21 @@ package simpledb.jdbc.embdded
 
 import simpledb.jdbc.ConnectionAdapter
 import simpledb.server.SimpleDB
+import java.sql.SQLException
 import java.sql.Statement
 
 class EmbeddedConnection(
     private val db: SimpleDB,
-): ConnectionAdapter() {
+) : ConnectionAdapter() {
     var currentTransaction = db.newTransaction()
     private val planner = db.planner
 
-    override fun createStatement(resultSetType: Int, resultSetConcurrency: Int, resultSetHoldability: Int): Statement {
+    override fun createStatement(): Statement {
         return EmbeddedStatement(this, planner)
+    }
+
+    override fun createStatement(resultSetType: Int, resultSetConcurrency: Int, resultSetHoldability: Int): Statement {
+        throw SQLException("operation not implemented")
     }
 
     override fun close() {
