2d662ee667d6a558467cd3788b9389d214b8c170
keyem4251 <kisk.emura@gmail.com>
2022-03-23 22:55:58 +0900

    change git ignore name

--- src/test/kotlin/metadata/TableManagerTest.kt
+++ src/test/kotlin/metadata/TableManagerTest.kt
@@ -1,4 +1,36 @@
 package metadata
 
+import org.junit.Test
+import simpledb.metadata.TableManager
+import simpledb.record.Schema
+import simpledb.server.SimpleDB
+
 class TableManagerTest {
+    @Test
+    fun testTableManager() {
+        val db = SimpleDB("test/tablemanagertest", 400, 8)
+        val transaction = db.newTransaction()
+        val tableManager = TableManager(true, transaction)
+
+        val schema = Schema()
+        schema.addIntField("A")
+        schema.addStringField("B", 9)
+        tableManager.createTable("MyTable", schema, transaction)
+
+        val layout = tableManager.getLayout("MyTable", transaction)
+        val size = layout.slotSize()
+        val schema2 = layout.schema()
+        println("MyTable has slot size $size")
+        println("Its fields are:")
+        for (fieldName in schema2.fields) {
+            val type = if (schema2.type(fieldName) == java.sql.Types.INTEGER) {
+                "int"
+            } else {
+                val stringLen = schema2.length(fieldName)
+                "varchar($stringLen)"
+            }
+            println("$fieldName: $type")
+        }
+        transaction.commit()
+    }
 }
